644cb7d0dffac68a6cb352a0c96ec6f31f8bb4f5
Jyestha <jyestha47@gmail.com>
2022-09-19 22:47:55 +0700

    GDRoom_D_10918

---
+++ app/src/main/java/com/jyestha/gdroom_d_10918/room/Constant.kt
@@ -0,0 +1,9 @@
+package com.jyestha.gdroom_d_10918.room
+
+class Constant {
+    companion object{
+        const val TYPE_READ = 0
+        const val TYPE_CREATE = 1
+        const val TYPE_UPDATE = 2
+    }
+}
---
+++ app/src/main/java/com/jyestha/gdroom_d_10918/room/Note.kt
@@ -0,0 +1,12 @@
+package com.jyestha.gdroom_d_10918.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val note: String
+)
